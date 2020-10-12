4cfe689d2cca3f9c1bb50611e3e071c0785ee2c8
KojoHero <60008545+KojoHero@users.noreply.github.com>
2020-10-12 17:36:03 +0000

    Update three

--- MyApp/forms.py
+++ MyApp/forms.py
@@ -1,4 +1,4 @@
-from django.forms import ModelForm
+from django.forms import ModelForm, PasswordInput
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.models import User
 from django import forms
@@ -8,6 +8,14 @@ class CreateUserForm(UserCreationForm):
     class Meta:
         model = User
         fields = ['username', 'email', 'password1', 'password2']
+        widgets = {
+            'username': forms.TextInput(attrs={'placeholder': 'Username'}),
+            'email': forms.EmailInput(attrs={'placeholder': 'Enter your email'}),
+            'password1': forms.PasswordInput(attrs={'placeholder': 'Password'}),
+            'password2': forms.PasswordInput(attrs={'placeholder': 'Confirm Password'}),
+        }
+
+
 
 
 # class ContactForm(forms.Form):
