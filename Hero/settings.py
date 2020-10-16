addc8de7c39b1f9afc2fbffb6d766d76709def50
KojoHero <60008545+KojoHero@users.noreply.github.com>
2020-10-16 16:43:57 +0000

    heroku updates

--- Hero/settings.py
+++ Hero/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'fg8^%5jup6$n26oq18!((a_klcys8wb)bo$7t#kpih1jh=@ebw'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['hero-kojo.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
